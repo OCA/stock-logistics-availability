c523b54787490542b29cd7b1041336223143ece4
Denis Roussel <denis.roussel@acsone.eu>
2023-11-13 23:15:28 +0100

    [IMP] stock_available_base_exclude_location: Use a Char domain field for UI

--- stock_available_base_exclude_location/models/stock_exclude_location_mixin.py
+++ stock_available_base_exclude_location/models/stock_exclude_location_mixin.py
@@ -1,7 +1,7 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.tools.safe_eval import safe_eval
 
 
 class StockExcludeLocationMixin(models.AbstractModel):
@@ -18,7 +18,20 @@ class StockExcludeLocationMixin(models.AbstractModel):
         "quantities.",
     )
     stock_excluded_location_domain = fields.Binary(
-        attachment=False,
+        compute="_compute_stock_excluded_location_domain",
         help="Fill in this with the domain you want to exclude locations for "
         "product available quantities",
     )
+    stock_excluded_location_domain_char = fields.Char(
+        default="[]",
+        string="Domain to filter locations",
+        help="Fill in this with the domain you want to exclude locations for "
+        "product available quantities",
+    )
+
+    @api.depends("stock_excluded_location_domain_char")
+    def _compute_stock_excluded_location_domain(self):
+        for record in self:
+            record.stock_excluded_location_domain = safe_eval(
+                record.stock_excluded_location_domain_char
+            )
