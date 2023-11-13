8bb6a8f958b003cfcf810b7d6b02e1abb2dad253
Denis Roussel <denis.roussel@acsone.eu>
2023-11-13 20:31:36 +0100

    [IMP] stock_available_base_exclude_location: Don't put base domain twice

--- stock_available_base_exclude_location/models/product_product.py
+++ stock_available_base_exclude_location/models/product_product.py
@@ -25,49 +25,75 @@ class ProductProduct(models.Model):
         )
         excluded_location_ids = self.env.context.get("excluded_location_ids")
         excluded_location_domain = self.env.context.get("excluded_location_domain")
+        domain_excluded_quant_loc = []
+        domain_excluded_move_in_loc = []
+        domain_excluded_move_out_loc = []
         if excluded_location_ids:
-            domain_quant_loc = expression.AND(
+            domain_excluded_quant_loc = expression.AND(
                 [
                     [("location_id", "not in", excluded_location_ids.ids)],
-                    domain_quant_loc,
+                    domain_excluded_quant_loc,
                 ]
             )
-            domain_move_in_loc = expression.AND(
-                [
-                    [
-                        (
-                            "location_dest_id",
-                            "not in",
-                            excluded_location_ids.ids,
-                        )
-                    ],
-                    domain_move_in_loc,
-                ]
-            )
-            domain_move_out_loc = expression.AND(
+            domain_excluded_move_in_loc = expression.AND(
                 [
                     [("location_id", "not in", excluded_location_ids.ids)],
-                    domain_move_out_loc,
+                    domain_excluded_move_in_loc,
+                ]
+            )
+            domain_excluded_move_out_loc = expression.AND(
+                [
+                    [("location_id", "not in", excluded_location_ids.ids)],
+                    domain_excluded_move_out_loc,
                 ]
             )
         if excluded_location_domain:
-            excluded_location_domain = excluded_location_domain
-            domain_quant_loc = expression.AND(
+            domain_excluded_quant_loc = expression.AND(
                 [
                     excluded_location_domain,
+                    domain_excluded_quant_loc,
+                ]
+            )
+            domain_excluded_move_in_loc = expression.AND(
+                [
+                    excluded_location_domain,
+                    domain_excluded_move_in_loc,
+                ]
+            )
+            domain_excluded_move_out_loc = expression.AND(
+                [
+                    excluded_location_domain,
+                    domain_excluded_move_out_loc,
+                ]
+            )
+        domain_quant_loc = (
+            expression.AND(
+                [
+                    domain_excluded_quant_loc,
                     domain_quant_loc,
                 ]
             )
-            domain_move_in_loc = expression.AND(
+            if domain_excluded_quant_loc
+            else domain_quant_loc
+        )
+        domain_move_in_loc = (
+            expression.AND(
                 [
-                    excluded_location_domain,
-                    domain_quant_loc,
+                    domain_excluded_move_in_loc,
+                    domain_move_in_loc,
                 ]
             )
-            domain_move_out_loc = expression.AND(
+            if domain_excluded_move_in_loc
+            else domain_move_in_loc
+        )
+        domain_move_out_loc = (
+            expression.AND(
                 [
-                    excluded_location_domain,
-                    domain_quant_loc,
+                    domain_excluded_move_out_loc,
+                    domain_move_out_loc,
                 ]
             )
+            if domain_excluded_move_in_loc
+            else domain_move_out_loc
+        )
         return domain_quant_loc, domain_move_in_loc, domain_move_out_loc
