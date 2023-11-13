25519a15b2460a863e016ae5d5840b3c7ac6c6ca
Denis Roussel <denis.roussel@acsone.eu>
2023-11-13 20:31:36 +0100

    [IMP] stock_available_base_exclude_location: Don't put base domain twice

--- stock_available_base_exclude_location/tests/test_stock_exclude_location.py
+++ stock_available_base_exclude_location/tests/test_stock_exclude_location.py
@@ -4,6 +4,9 @@ from odoo_test_helper import FakeModelLoader
 
 from odoo.tests.common import TransactionCase
 
+from odoo.addons.stock.models.stock_location import Location
+from odoo.addons.stock.models.stock_move import StockMove
+
 
 class TestExcludeLocation(TransactionCase):
     @classmethod
@@ -27,6 +30,23 @@ class TestExcludeLocation(TransactionCase):
     def tearDownClass(cls):
         cls.loader.restore_registry()
         super().tearDownClass()
+
+    @classmethod
+    def _create_stock_move(
+        cls, quantity: float, location: Location, location_dest: Location
+    ) -> StockMove:
+        move = cls.env["stock.move"].create(
+            {
+                "name": "Move",
+                "location_id": location.id,
+                "location_dest_id": location_dest.id,
+                "product_id": cls.product.id,
+                "product_uom_qty": quantity,
+                "product_uom": cls.product.uom_id.id,
+            }
+        )
+        move._action_confirm()
+        return move
 
     def _add_stock_to_product(self, product, location, qty):
         """
@@ -74,6 +94,29 @@ class TestExcludeLocation(TransactionCase):
             excluded_location_domain=self.fake.stock_excluded_location_domain
         ).qty_available
         self.assertEqual(50.0, qty)
+        self.product.invalidate_recordset()
+        qty = self.product.qty_available
+        self.assertEqual(75.0, qty)
+
+    def test_exclude_location_domain_in(self):
+        # Add different levels of stock for product as :
+        # Shop 0: 50.0
+        # Sub Level (Shop 0 / Sub Location 1): 25.0
+        # Query product stock availability normally and with excluded
+        # location as Supplier
+        self._add_stock_to_product(self.product, self.location_shop, 50.0)
+        self._add_stock_to_product(self.product, self.sub_location_1, 25.0)
+        self._create_stock_move(
+            30.0, self.env.ref("stock.stock_location_suppliers"), self.sub_location_1
+        )
+        self.product.invalidate_recordset()
+        qty = self.product.with_context(
+            excluded_location_domain=self.fake.stock_excluded_location_domain
+        ).virtual_available
+        self.assertEqual(105.0, qty)
+        self.fake.stock_excluded_location_domain = [
+            ("location_id.usage", "!=", "supplier")
+        ]
         self.product.invalidate_recordset()
         qty = self.product.qty_available
         self.assertEqual(75.0, qty)
