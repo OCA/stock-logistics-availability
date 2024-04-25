40c0a1639f7eae2aafc122c35d1ce1e44f2da307
Ruchir Shukla <ruchir@bizzappdev.com>
2024-04-25 19:06:39 +0530

    [MIG] sale_stock_available_info_popup: Migration to 17.0

--- sale_stock_available_info_popup/__manifest__.py
+++ sale_stock_available_info_popup/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-availability",
     "category": "Warehouse Management",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale_stock", "stock_available"],
     "data": ["views/sale_order_views.xml"],
