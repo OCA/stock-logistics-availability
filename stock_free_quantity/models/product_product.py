412c26267de0399e6ca2071ce9eeed728d736617
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-10-08 10:54:15 -0300

    [IMP] : pre-commit auto fixes

--- stock_free_quantity/models/product_product.py
+++ stock_free_quantity/models/product_product.py
@@ -1,13 +1,9 @@
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
-
-from odoo.addons.stock.models.product import OPERATORS
+from odoo import models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    
     def _prepare_domain_available_not_reserved(self):
         domain_quant = [("product_id", "in", self.ids)]
         domain_quant_locations = self._get_domain_locations()[0]
