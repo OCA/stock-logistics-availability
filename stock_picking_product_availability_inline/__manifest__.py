66bc8194da3f7b0284c2b49ce7b2fe8e0f76380c
Carlos Roca <carlos.roca@tecnativa.com>
2025-01-23 11:36:33 +0100

    [MIG] stock_picking_product_availability_inline: Migration to 18.0

--- stock_picking_product_availability_inline/__manifest__.py
+++ stock_picking_product_availability_inline/__manifest__.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Product Availability Inline",
-    "summary": "Show product availability in product drop-down in stock picking form view.",
-    "version": "15.0.1.0.1",
-    "development_status": "Beta",
+    "summary": "Show product availability in product drop-down of picking form view.",
+    "version": "18.0.1.0.0",
+    "development_status": "Production/Stable",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-availability",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
+    "maintainers": ["CarlosRoca13"],
     "depends": ["stock", "base_view_inheritance_extension"],
     "data": ["views/stock_picking_views.xml", "views/stock_move_line_views.xml"],
 }
