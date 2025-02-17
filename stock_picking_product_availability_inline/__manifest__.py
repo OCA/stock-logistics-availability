e099426aac60c9d605f024dda530131daa8ca194
Carlos Roca <carlos.roca@tecnativa.com>
2025-02-17 16:39:39 +0100

    [IMP] stock_picking_product_availability_inline: pre-commit auto fixes

--- stock_picking_product_availability_inline/__manifest__.py
+++ stock_picking_product_availability_inline/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "15.0.1.0.1",
     "development_status": "Beta",
     "category": "Warehouse Management",
-    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "website": "https://github.com/OCA/stock-logistics-availability",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["stock", "base_view_inheritance_extension"],
